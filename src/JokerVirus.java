8b23fbbc8250b930bd790861caad7eaf7433c99a
Abubakar Yusuf <abubakaryusuf@Abubakars-MacBook-Air.local>
2022-10-18 12:56:26 +0100

    first commit

---
+++ src/JokerVirus.java
@@ -0,0 +1,24 @@
+
+public class JokerVirus {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("**************************************************");
+		System.out.println("**\tJOKER VIRUS INFORMATION SYSTEM\t\t**");
+		System.out.println("**************************************************");
+		System.out.println();
+		System.out.println("SELECT AN OPTION");
+		System.out.println("1) VIEW STATS \n2) UPDATE STATS \n3) EXIT");
+		/* PROJECT EXERCISE
+		READ THE BELOW INSTRUCTION CAREFULLY:
+		
+		WRITE THE STATEMENTS THAT DO THE FOLLOWING
+		1. DECLEAR VARIABLES THAT STORE THE NUMBER OF VICTIMS, ON ADMISSION, DISCHARGED AND DEAD IN NORTH, SOUTH AND EAST RIGBEE
+		2. DECLEAR VARIABLES THAT STORE THE TOTALS OF (VICTIMS ON ADMISSION, DISCHARGED AND DEAD)		
+		*/
+		
+		
+		
+	}
+
+}
